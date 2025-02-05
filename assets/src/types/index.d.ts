fc6fbcf5f2312e60ab2e752668511a45b28aa2b4
yinminc <a1598742002@gmail.com>
2025-02-05 18:15:31 +0800

    Release version: 0.1.5

---
+++ assets/src/types/index.d.ts
@@ -0,0 +1,4 @@
+export * from './user-segment-map';
+export * from './user';
+export * from './option';
+export * from './question';
---
+++ assets/src/types/option.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Part of melo project.
+ *
+ * @copyright  Copyright (C) 2024 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+export interface Option {
+  id: number;
+  title: string;
+  isAnswer: boolean;
+  ordering: number;
+}
