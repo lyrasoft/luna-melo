8bdd5007a26650ef944de3c9585a129b807c013d
yinminc <a1598742002@gmail.com>
2025-02-07 12:25:52 +0800

    fix profile sidebar

--- views/melo/front/profile-sidebar.blade.php
+++ views/melo/front/profile-sidebar.blade.php
@@ -29,47 +29,54 @@ $menuHelper = $app->service(MenuHelper::class);
 $lessonService = $app->service(LessonService::class);
 ?>
 
-<div class="c-user-card card border-0 shadow rounded-4">
+<div class="card border-0 shadow rounded-4">
     <div class="card-body text-center">
-        <img src="{{ $info->avatar }}" class="c-user-card__avatar rounded-pill mb-4" alt="{{ $info->name }}">
-        <div class="card-title h4">{{ $info->name }}</div>
+        <img src="{{ $info->avatar }}" class="rounded-pill mb-4" alt="{{ $info->name }}" width="100">
+        <div class="card-title h4">
+            {{ $info->name }}
+        </div>
     </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item px-0">
-            <a class="d-flex justify-content-between {{ $menuHelper->active('profile_edit') }}"
-                href="{{ $nav->to('profile_edit') }}">
-                <div><i class="far fa-user-check fa-fw me-2"></i>帳號管理</div>
-            </a>
-        </li>
-        <li class="list-group-item px-0">
-            <a class="d-flex justify-content-between {{ $menuHelper->active('my_lesson') }}"
-                href="{{ $nav->to('my_lesson_list') }}">
-                <div>
-                    <i class="far fa-book-open fa-fw me-2"></i>我的課程
-                </div>
-                <div>
-                    {{ $lessonService->myLessonCount() }}
-                </div>
-            </a>
-        </li>
-        <li class="list-group-item px-0">
-            <a class="d-flex justify-content-between {{ $menuHelper->active('my_lecture') }}"
-                href="{{ $nav->to('my_lecture_list') }}">
-                <div>
-                    <i class="far fa-chalkboard fa-fw me-2"></i>我開的課
-                </div>
-                <div>
-                    {{ $lessonService->myLectureCount() }}
-                </div>
-            </a>
-        </li>
-        <li class="list-group-item px-0">
-            <a class="d-flex align-items-center" href="{{ $nav->to('logout') }}">
+    <div class="list-group list-group-flush">
+        <a class="d-flex justify-content-between list-group-item list-group-item-action {{ $menuHelper->active('profile_edit') }}"
+            href="{{ $nav->to('profile_edit') }}">
+            <div>
+                <i class="far fa-user-check fa-fw me-2"></i>
+                帳號管理
+            </div>
+        </a>
+        <a class="d-flex justify-content-between list-group-item list-group-item-action {{ $menuHelper->active('my_lesson') }}"
+            href="{{ $nav->to('my_lesson_list') }}">
+            <div>
+                <i class="far fa-book-open fa-fw me-2"></i>
+                我的課程
+            </div>
+            <div>
+                {{ $lessonService->myLessonCount() }}
+            </div>
+        </a>
+        <a class="d-flex justify-content-between list-group-item list-group-item-action {{ $menuHelper->active('my_lecture') }}"
+            href="{{ $nav->to('my_lecture_list') }}">
+            <div>
+                <i class="far fa-chalkboard fa-fw me-2"></i>
+                我開的課
+            </div>
+            <div>
+                {{ $lessonService->myLectureCount() }}
+            </div>
+        </a>
+        <a class="d-flex align-items-center list-group-item list-group-item-action"
+            href="{{ $nav->to('logout') }}">
+            <div>
                 <i class="far fa-sign-out fa-fw me-2"></i>
-                <div>
-                    登出
-                </div>
-            </a>
+                登出
+            </div>\
+        </a>
+    </div>
+
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+
         </li>
+
     </ul>
 </div>
