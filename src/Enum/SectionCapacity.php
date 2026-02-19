4a66ebee2b515aa5f76c741005ccdba4aa79ef77
Simon Asika <asika32764@gmail.com>
2026-02-19 20:26:36 +0800

    capacity title

--- src/Enum/SectionCapacity.php
+++ src/Enum/SectionCapacity.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lyrasoft\Melo\Enum;
 
+use Windwalker\Utilities\Attributes\Enum\Title;
 use Windwalker\Utilities\Enum\EnumRichInterface;
 use Windwalker\Utilities\Enum\EnumRichTrait;
 
@@ -11,10 +12,19 @@ enum SectionCapacity implements EnumRichInterface
 {
     use EnumRichTrait;
 
+    #[Title('可觀看')]
     case AVAILABLE;
+
+    #[Title('可預覽')]
     case PREVIEW;
+
+    #[Title('請先登入')]
     case LOGIN_REQUIRED;
+
+    #[Title('尚未擁有此課程')]
     case NOT_YET_ATTENDED;
+
+    #[Title('進度未達')]
     case PROGRESS_LOCKED;
 
     protected function translateKey(string $name): string
