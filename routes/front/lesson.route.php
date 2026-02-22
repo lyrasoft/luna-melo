b57971b8b2f36c589277deb6d6886bf5a430d23c
Simon Asika <asika32764@gmail.com>
2026-02-22 16:39:28 +0800

    section controller

--- routes/front/lesson.route.php
+++ routes/front/lesson.route.php
@@ -7,6 +7,7 @@ namespace App\Routes;
 use Lyrasoft\Melo\Module\Front\Lesson\LessonController;
 use Lyrasoft\Melo\Module\Front\Lesson\LessonItemView;
 use Lyrasoft\Melo\Module\Front\Lesson\LessonListView;
+use Lyrasoft\Melo\Module\Front\Segment\SectionController;
 use Windwalker\Core\Router\RouteCreator;
 
 /** @var RouteCreator $router */
@@ -19,6 +20,12 @@ $router->group('lesson')
         $router->any('lesson_item', '/lesson/{id:\d+}[-{alias}[/attend/{segment_id}]]')
             ->view(LessonItemView::class);
 
+        $router->any('section_file', '/section/file/{id}')
+            ->controller(SectionController::class, 'file');
+
         $router->any('lesson_ajax', '/lesson/ajax[/{task}]')
             ->controller(LessonController::class, 'ajax');
+
+        $router->any('section_ajax', '/section/ajax[/{task}]')
+            ->controller(SectionController::class, 'ajax');
     });
